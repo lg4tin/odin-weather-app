1dbc72f886cf3758d87a6a13fe25bb32b7129efa
lg4tin <lg4tin@gmail.com>
2023-10-18 11:54:36 -0400

    Add styling to the app

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 /*eslint-disable*/
 
 
-const img = document.querySelector('img');
-    async function getWeather(city) {
-      const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=01e585a4ff61420c823232424231610&q=${city}`, {mode: 'cors'})
-        
-      const weatherData = await response.json()
-      //img.src = catData.data.images.original.url;
-      console.log(weatherData.current.temp_f, weatherData.current.temp_c)
-    }
+
+async function getWeather(city) {
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=01e585a4ff61420c823232424231610&q=${city}`, {mode: 'cors'})
+    
+  const weatherData = await response.json();
+  //const data = await Object.entries(weatherData.current);
+
+  //console.log(data[2])
+
+  console.log(weatherData.current.temp_f, weatherData.current.temp_c, weatherData.current.feelslike_f, weatherData.current.humidity, weatherData.current.wind_mph, weatherData.location.name)
+
+  return weatherData;
+  
+}
+
+async function siftData(obj) {
+  let filteredObj = obj.filter(data => data[0] == 'temp_f');
+  return filteredObj;
+}
